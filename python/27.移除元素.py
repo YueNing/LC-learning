0308f6b42d3c9972b8e2019b758a252bd787a771
Yue <n1085633848@outlook.com>
2019-12-27 20:03:51 +0100

    27.移除元素

---
+++ python/27.移除元素.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start], nums[end], end = nums[end], nums[start], end - 1
+            else:
+                start +=1
+        return start
+
+# @lc code=end
+
